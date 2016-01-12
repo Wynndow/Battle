60f47999e6262f01860568dd4fe23722ddb19ef1
Chris Wynne <chris.wynndow@gmail.com>
2016-01-12 19:07:30 +0000

    Attacks reduce HP

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,21 @@
 class Player
 
-  attr_reader :name
-  
-  def initialize(name: )
+  attr_reader :name, :hit_points
+
+  STARTING_HIT_POINTS = 150
+  DEFAULT_ATTACK = 20
+
+  def initialize(name:, hit_points: STARTING_HIT_POINTS)
     @name = name
+    @hit_points = hit_points
+  end
+
+  def attack(player)
+    player.receive_damage
+  end
+
+  def receive_damage
+    @hit_points -= DEFAULT_ATTACK
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,31 @@
 require 'player'
 
 describe Player do
-  subject(:player) {described_class.new(name: "Vic")}
+  subject(:vic) {described_class.new(name: "Vic")}
+  subject(:bob) {described_class.new(name: "Bob")}
 
   describe '#name' do
     it 'returns it\'s name' do
-      expect(player.name).to eq("Vic")
+      expect(vic.name).to eq("Vic")
+    end
+  end
+
+  describe 'by default' do
+    it 'has hit points' do
+      expect(vic.hit_points).to eq(Player::STARTING_HIT_POINTS)
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(bob).to receive(:receive_damage)
+      vic.attack(bob)
+    end
+  end
+
+  describe '#recieve_damage' do
+    it 'reduces a player\'s HP' do
+      expect{ vic.receive_damage }.to change{ vic.hit_points }.by(-Player::DEFAULT_ATTACK)
     end
   end
 
