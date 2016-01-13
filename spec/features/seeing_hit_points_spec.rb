49b5d5038b5e905314dd7ad403bac35305fd78af
Chris Wynne <chris.wynndow@gmail.com>
2016-01-13 09:25:08 +0000

    Adds P1 HP to play view

--- spec/features/seeing_hit_points_spec.rb
+++ spec/features/seeing_hit_points_spec.rb
@@ -5,4 +5,9 @@ feature 'Seeing hit points' do
     expect(page).to have_content('Bob\'s HP: 150')
   end
 
+  scenario 'shows player 1\s hit points' do
+    sign_in
+    expect(page).to have_content('Vic\'s HP: 150')
+  end
+
 end
